c21e61ed601af90624bad6430a0cfd9b3834b468
Luis <luisfernandosantaospina@gmail.com>
2022-11-17 08:29:25 -0500

    Ajusta scrip

--- script.py
+++ script.py
@@ -2,13 +2,13 @@ import os
 import subprocess
 
 for i in range(1, 3):
-    os.system('cp -rl cypress-project_1/results/ESCP' + str(i) +
-              '* cypress-project_2/results/ESCP' + str(i) +
+    os.system('cp -rl cypress-project-ghost-3-4-4-vrt/results/ESCP' + str(i) +
+              '* cypress-project-ghost-4-0-0-vrt/results/ESCP' + str(i) +
               '* resemble-project/results/')
 
 for i in range(3, 5):
-    os.system('cp -rl kraken-project_1/results/ESCP' + str(i) +
-              '* kraken-project_2/results/ESCP' + str(i) +
+    os.system('cp -rl kraken-project-ghost-3-4-4-vrt/results/ESCP' + str(i) +
+              '* kraken-project-ghost-4-0-0-vrt/results/ESCP' + str(i) +
               '* resemble-project/results/')
 
 os.chdir('resemble-project')
