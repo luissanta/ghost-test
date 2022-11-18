eef59499324df4c2947a9c60b3cf9b18d46f317a
Luis <luisfernandosantaospina@gmail.com>
2022-11-17 19:52:29 -0500

    Descomenta codigo

--- script.py
+++ script.py
@@ -1,31 +1,31 @@
 import os
 
 # Instala dependencias en todos los proyectos
-# projects = [
-#     'cypress-project-ghost-3-41-1-vrt',
-#     'cypress-project-ghost-4-44-0-vrt',
-#     'kraken-project-ghost-3-41-1-vrt',
-#     'kraken-project-ghost-4-44-0-vrt',
-#     'resemble-project'
-# ]
-#
-# for project in projects:
-#     os.chdir(project)
-#     os.system('npm install')
-#     os.chdir('..')
-#
-#
-# # Ejecuta todos los test en ambas versiones de kraken
+projects = [
+    'cypress-project-ghost-3-41-1-vrt',
+    'cypress-project-ghost-4-44-0-vrt',
+    'kraken-project-ghost-3-41-1-vrt',
+    'kraken-project-ghost-4-44-0-vrt',
+    'resemble-project'
+]
+
+for project in projects:
+    os.chdir(project)
+    os.system('npm install')
+    os.chdir('..')
+
+
+# Ejecuta todos los test en ambas versiones de kraken
 kraken_directories = ['kraken-project-ghost-3-41-1-vrt', 'kraken-project-ghost-4-44-0-vrt']
-#
-# for kraken_directory in kraken_directories:
-#     kraken_scenarios = os.listdir(kraken_directory + '/scenarios')
-#     for kraken_scenario in kraken_scenarios:
-#         os.system('cp ' + kraken_directory + '/scenarios/' + kraken_scenario + ' ' + kraken_directory + '/features')
-#         os.chdir(kraken_directory)
-#         os.system('npx kraken-node run')
-#         os.chdir('..')
-#         os.system('rm -r ' + kraken_directory + '/features/' + kraken_scenario)
+
+for kraken_directory in kraken_directories:
+    kraken_scenarios = os.listdir(kraken_directory + '/scenarios')
+    for kraken_scenario in kraken_scenarios:
+        os.system('cp ' + kraken_directory + '/scenarios/' + kraken_scenario + ' ' + kraken_directory + '/features')
+        os.chdir(kraken_directory)
+        os.system('npx kraken-node run')
+        os.chdir('..')
+        os.system('rm -r ' + kraken_directory + '/features/' + kraken_scenario)
 
 
 # Extrae todos los screenshots de kraken para ser analizados por resemble
